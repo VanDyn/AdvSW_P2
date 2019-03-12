e5dfb9f29352d4d3dc4f0582751412e1951587ba
jrome5 <35452154+jrome5@users.noreply.github.com>
2019-03-12 19:54:59 +0000

    Update CafeQueue.java

--- Code/Part_2/src/main/CafeQueue.java
+++ Code/Part_2/src/main/CafeQueue.java
@@ -1,13 +1,24 @@
 package main;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Observer;
 import java.util.Queue;
 
-public class CafeQueue extends Thread{
+import main.Order;
+import main.OrderList;
 
-	private Queue<Object> queue;
+public class CafeQueue implements Runnable{
+
+	private static Queue<Order> queue;
+	private List<Observer> registeredObservers = new ArrayList<Observer>();
 	
 	CafeQueue(){
 		
 	}
+	
+
 	
 	/*
 	 * Run method for the thread.
@@ -16,9 +27,15 @@ public class CafeQueue extends Thread{
 	 * @see java.lang.Thread#run()
 	 */
 	public void run() {
-		while(OrderList.getSize != 0 && queue.size() != 0) {
+		while(Interface.getSize() != 0 && queue.size() != 0) {
 			addToQueue();
-			sleep(50);
+			notifyObservers();
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	
@@ -26,8 +43,8 @@ public class CafeQueue extends Thread{
 	 * Add an order to the queue
 	 */
 	private synchronized void addToQueue() {
-		Order order = OrderList.getOrder();
-		sendToLog("%s has joined the queue", order.getName());
+		Order order = Interface.getOrder();
+		sendToLog("%s has joined the queue", order.getID());
 		queue.add(order);
 	}
 	
@@ -35,15 +52,36 @@ public class CafeQueue extends Thread{
 	 * Method for the server thread to access orders from the queue.
 	 * Accessed orders will be removed from the queue.
 	 */
-	public synchronized Object serveCustomer() {
+	public static synchronized Order serveCustomer() {
 		
-		sendToLog("Customer Served : %s", queue.peek().getName());
+		sendToLog("Customer Served : %s", queue.peek().getID());
 		return queue.poll();
 	}
 	
 	// Sends a string to the Log class
 	private synchronized void sendToLog(String details) {
 		Log log = Log.getInstance(details);
+	}
+
+	public void registerObserver(Observer obs) {
+		registeredObservers.add(obs);
+		
+	}
+
+	public void removeObserver(Observer obs) {
+		registeredObservers.remove(obs);
+		
+	}
+
+	public void notifyObservers() {
+		for(Observer obs : registeredObservers) {
+			obs.update();
+		}
+		
+	}
+	
+	public static Queue getQueue() {
+		return queue;
 	}
 	
 }
